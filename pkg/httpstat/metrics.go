39664df4fd7eb31dfe5992e2d6a0c71c7fd09bbe
rdeusser <robertjdeusser@gmail.com>
2021-03-13 20:54:30 -0500

    feat: add metrics

--- pkg/httpstat/metrics.go
+++ pkg/httpstat/metrics.go
@@ -6,5 +6,13 @@ import (
 )
 
 var (
-	urlUp = promauto.NewGauge(prometheus.GaugeOpts{})
+	urlUp = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "sample_external_url_up",
+		Help: "Is the URL up?",
+	})
+
+	urlResponseMS = promauto.NewHistogram(prometheus.HistogramOpts{
+		Name: "sample_external_url_response_ms",
+		Help: "URL response time in milliseconds",
+	})
 )
